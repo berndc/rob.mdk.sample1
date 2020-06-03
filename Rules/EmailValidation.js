613199117f0849c42ee011c89d8b0722c0389fd5
Robert Hunger <robert.hunger@mode-it-systems.de>
2020-06-03 19:04:19 +0000

    new Git 2

--- Rules/EmailValidation.js
+++ Rules/EmailValidation.js
@@ -1,6 +1,6 @@
 export default function EmailValidation(ClientAPI) {
 
-	console.debug("Rules: Name ValidITON")
+	console.debug("---> Rules: Name ValidITON - 1")
 
 	//The following evaluateTargetPath will retrieve the current value of the email control
 	if (ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyName/#Value')) {
