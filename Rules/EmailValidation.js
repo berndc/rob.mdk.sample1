d43326cd672265578bdf1a000badcafbe666cf5d
Robert Hunger <robert.hunger@mode-it-systems.de>
2020-06-03 18:09:11 +0000

    Change Name on Edit Page

---
+++ Rules/EmailValidation.js
@@ -0,0 +1,25 @@
+export default function EmailValidation(ClientAPI) {
+
+	console.debug("Rules: Name ValidITON")
+
+	//The following evaluateTargetPath will retrieve the current value of the email control
+	if (ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value')) {
+
+		var fieldValue = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value');
+		var element = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel');
+
+		if (fieldValue == 'X') {
+			//element.setEditable(false);
+			element.setStyle("Background", "objectInputFaild");
+			element.redraw();
+
+		}
+
+		//atSign.executeAction('/RobSampleApp1/Actions/ValidationFailure.action');
+	} else {
+
+		//If @ is present in the email value, return true to indicate validation is successful
+		return true;
+	}
+
+}
---
+++ Rules/_checkInput.js
@@ -0,0 +1,22 @@
+export default function checkInput(ClientAPI) {
+
+	//The following evaluateTargetPath will retrieve the current value of the email control
+	if (ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value')) {
+
+		var fieldValue = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value');
+		var element = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel');
+
+		if (fieldValue == 'X') {
+			//element.setEditable(false);
+			element.setStyle("Background", "objectInputFaild");
+			element.redraw();
+
+		}
+
+		//atSign.executeAction('/RobSampleApp1/Actions/ValidationFailure.action');
+	} else {
+
+		//If @ is present in the email value, return true to indicate validation is successful
+		return true;
+	}
+}
