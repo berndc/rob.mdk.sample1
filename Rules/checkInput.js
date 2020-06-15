2791f78649c695a07756d12ce0a96a56ad6b628b
Robert Hunger <robert.hunger@mode-it-systems.de>
2020-06-15 09:12:08 +0000

    something with name

--- Rules/checkInput.js
+++ Rules/checkInput.js
@@ -1,23 +1,33 @@
 export default function checkInput(ClientAPI) {
 
 	//The following evaluateTargetPath will retrieve the current value of the email control
-	if (ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value')) {
+	if (!ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value')) {
 
 		var fieldValue = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel/#Value');
 		var element = ClientAPI.evaluateTargetPath('#Page:EditNamePage/#Control:FCmyNamel');
 
-		if (fieldValue == 'X') {
-			//element.setEditable(false);
-			//element.setStyle("Background", "objectInputFaild");
-			
-			element.setValue("Bitte Wert eingeben");
-			element.redraw();
-            
-            return false;
-		}
+		console.error(">>es wurde kein Name eingeben");
+
+		ClientAPI.setStyle("objectInputFaild", "");
+
+		ClientAPI.redraw();
+
+		return false;
 
 		//atSign.executeAction('/RobSampleApp1/Actions/ValidationFailure.action');
 	} else {
+
+		let myName = ClientAPI.getGlobalDefinition('/MDKSample1/Globals/myName.global');
+
+       ClientAPI.setStyle("objectInput", "");
+
+		if (myName.getType() == 'string') {
+			alert(myName.getValue());
+		}
+		
+		ClientAPI.redraw();
+		
+		//do something
 
 		//If @ is present in the email value, return true to indicate validation is successful
 		return true;
